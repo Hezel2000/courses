d069a6c68b3084f5035a3ae05b1e3ea56e0bf2e7
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2023-03-20 20:46:22 +0100

    x

--- pages/01_Data Science.py
+++ pages/01_Data Science.py
@@ -34,48 +34,48 @@ def importCourseDatasheet():
     return dfSearchAll
 
 
-def useCourse(dfSearchAll):
-    dfSearchAll = dfSearchAll
-    gd = GridOptionsBuilder.from_dataframe(dfSearchAll)
-    gd.configure_pagination(enabled=True)
-    gd.configure_default_column(editable=True,groupable=True)
-    gd.configure_selection(selection_mode='single', use_checkbox=True)
-    gridoptions = gd.build()
-    grid_table = AgGrid(dfSearchAll, gridOptions=gridoptions, update_mode = GridUpdateMode.SELECTION_CHANGED, theme='material')
-    sel_row = grid_table['selected_rows']
+# def useCourse(dfSearchAll):
+#     dfSearchAll = dfSearchAll
+#     gd = GridOptionsBuilder.from_dataframe(dfSearchAll)
+#     gd.configure_pagination(enabled=True)
+#     gd.configure_default_column(editable=True,groupable=True)
+#     gd.configure_selection(selection_mode='single', use_checkbox=True)
+#     gridoptions = gd.build()
+#     grid_table = AgGrid(dfSearchAll, gridOptions=gridoptions, update_mode = GridUpdateMode.SELECTION_CHANGED, theme='material')
+#     sel_row = grid_table['selected_rows']
 
-    if len(sel_row) > 0:    
+#     if len(sel_row) > 0:    
         
-        col1, col2 = st.columns([3, 1])
-        with col1:
-            st.video(sel_row[0]['Youtube'])
-        with col2:
-            st.write('Laufzeit: ' + sel_row[0]['Laufzeit'])
-            with st.expander('Jupyter Notebooks', expanded=True):
-                if sel_row[0]['Vorlesung ipynb'] != 'none':
-                    vorlesung = "[Vorlesung](https://raw.githubusercontent.com/Hezel2000/Data_Science/main/jupyter_nb/" + sel_row[0]['Vorlesung ipynb'] + ")"
-                else:
-                    vorlesung=''
-                if sel_row[0]['Übungen ipynb'] != 'none':
-                    uebungen = "[Übungen](https://raw.githubusercontent.com/Hezel2000/Data_Science/main/jupyter_nb/" + sel_row[0]['Übungen ipynb'] + ")"
-                else:
-                    uebungen=''
-                if sel_row[0]['Lösungen ipynb'] != 'none':
-                    loesungen = "[Lösungen](https://raw.githubusercontent.com/Hezel2000/Data_Science/main/jupyter_nb/" + sel_row[0]['Lösungen ipynb'] + ")"
-                else:
-                    loesungen=''
-                if vorlesung=='' and uebungen=='' and loesungen=='':
-                    st.write('keine vorhanden')
-                else:
-                    st.write(vorlesung,uebungen,loesungen)
-            with st.expander('Schlagworte', expanded=True):
-                if sel_row[0]['Schlagworte'] != 'none':
-                    st.write(sel_row[0]['Schlagworte'])
-                else:
-                    st.write('keine vorhannden')
+#         col1, col2 = st.columns([3, 1])
+#         with col1:
+#             st.video(sel_row[0]['Youtube'])
+#         with col2:
+#             st.write('Laufzeit: ' + sel_row[0]['Laufzeit'])
+#             with st.expander('Jupyter Notebooks', expanded=True):
+#                 if sel_row[0]['Vorlesung ipynb'] != 'none':
+#                     vorlesung = "[Vorlesung](https://raw.githubusercontent.com/Hezel2000/Data_Science/main/jupyter_nb/" + sel_row[0]['Vorlesung ipynb'] + ")"
+#                 else:
+#                     vorlesung=''
+#                 if sel_row[0]['Übungen ipynb'] != 'none':
+#                     uebungen = "[Übungen](https://raw.githubusercontent.com/Hezel2000/Data_Science/main/jupyter_nb/" + sel_row[0]['Übungen ipynb'] + ")"
+#                 else:
+#                     uebungen=''
+#                 if sel_row[0]['Lösungen ipynb'] != 'none':
+#                     loesungen = "[Lösungen](https://raw.githubusercontent.com/Hezel2000/Data_Science/main/jupyter_nb/" + sel_row[0]['Lösungen ipynb'] + ")"
+#                 else:
+#                     loesungen=''
+#                 if vorlesung=='' and uebungen=='' and loesungen=='':
+#                     st.write('keine vorhanden')
+#                 else:
+#                     st.write(vorlesung,uebungen,loesungen)
+#             with st.expander('Schlagworte', expanded=True):
+#                 if sel_row[0]['Schlagworte'] != 'none':
+#                     st.write(sel_row[0]['Schlagworte'])
+#                 else:
+#                     st.write('keine vorhannden')
         
-        #st.subheader('Beschreibung')
-        st.write(sel_row[0]['Beschreibung'])
+#         #st.subheader('Beschreibung')
+#         st.write(sel_row[0]['Beschreibung'])
 
 # =============================================================================
 #         else:
@@ -83,9 +83,10 @@ def useCourse(dfSearchAll):
 # =============================================================================
         
 
-dfSearchAll = importCourseDatasheet()
+pd.read_csv('https://raw.githubusercontent.com/Hezel2000/Data_Science/main/course_material_data_science.csv')
+#dfSearchAll = importCourseDatasheet()
 
-useCourse(dfSearchAll)
+# useCourse(dfSearchAll)
 
 
     
