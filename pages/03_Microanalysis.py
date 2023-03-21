ccbcefb04f87f965b48572adeee48b1a77290e43
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2023-03-21 07:54:34 +0100

    x

--- pages/03_Microanalysis.py
+++ pages/03_Microanalysis.py
@@ -3,4 +3,75 @@ from st_aggrid import AgGrid, GridUpdateMode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 import pandas as pd
 
-st.subheader('Coming soon')
+import streamlit as st
+from st_aggrid import AgGrid, GridUpdateMode
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+import pandas as pd
+
+# =============================================================================
+# hide_st_style = """
+#             <style>
+#             #MainMenu {visibility: hidden;}
+#             footer {visibility: hidden;}
+#             header {visibility: hidden;}
+#             </style>
+#             """
+# st.markdown(hide_st_style, unsafe_allow_html=True)
+# =============================================================================
+
+    
+st.subheader('Willkommen zur Einführung in die Mikroanalytik')
+st.write('*für Mineralogen, Kosmo-/Geochemiker, Petrologen & den ganzen Rest*')
+
+
+#---------------------------------#
+#------ Vorlesungen & Übungen ----#
+#---------------------------------#
+#def Vorlesungen_Uebungen():
+import streamlit as st
+st.subheader('Wähle Deine Lerneinheit')
+
+@st.cache
+def importCourseDatasheet():
+    dfSearchAll= pd.read_csv('data/course_material_microanalysis.csv')
+    return dfSearchAll
+
+
+def useCourse(dfSearchAll):
+    dfSearchAll = dfSearchAll
+    gd = GridOptionsBuilder.from_dataframe(dfSearchAll)
+    gd.configure_pagination(enabled=True)
+    gd.configure_default_column(editable=True,groupable=True)
+    gd.configure_selection(selection_mode='single', use_checkbox=True)
+    gridoptions = gd.build()
+    grid_table = AgGrid(dfSearchAll, gridOptions=gridoptions, update_mode = GridUpdateMode.SELECTION_CHANGED, theme='material')
+    sel_row = grid_table['selected_rows']
+
+    if len(sel_row) > 0:    
+        
+        col1, col2 = st.columns([3, 1])
+        with col1:
+            st.video(sel_row[0]['youtube - deutsch'])
+        with col2:
+            st.write('Laufzeit: ' + sel_row[0]['Laufzeit'])
+            with st.expander('Schlagworte', expanded=True):
+                if sel_row[0]['Schlagworte'] != 'none':
+                    st.write(sel_row[0]['Schlagworte'])
+                else:
+                    st.write('keine vorhannden')
+        
+        #st.subheader('Beschreibung')
+        st.write(sel_row[0]['Beschreibung'])
+
+
+dfSearchAll = importCourseDatasheet()
+useCourse(dfSearchAll)
+
+    
+#---------------------------------#
+#------ Main Page Sidebar --------#
+#---------------------------------#  
+
+st.sidebar.image('data_cosmochemistry/Goethe-Logo.jpg', width=150)
+st.sidebar.write("Viele Wege führen zum Erfolg.")
+
