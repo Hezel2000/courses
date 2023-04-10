3ba33d34809fa9771daa20351d00df3d9f75d1f6
Dominik Hezel <dominik.hezel@em.uni-frankfurt.de>
2023-04-10 15:00:45 +0200

    x

--- Welcome.py
+++ Welcome.py
@@ -1,6 +1,8 @@
 import streamlit as st
-from st_aggrid import AgGrid, GridUpdateMode
-from st_aggrid.grid_options_builder import GridOptionsBuilder
+# from st_aggrid import AgGrid, GridUpdateMode
+# from st_aggrid.grid_options_builder import GridOptionsBuilder
 import pandas as pd
 
 st.subheader('Welcome')
+
+st.write('Über die Sidebar links erhälst Du Zugriff auf derzeit 3 verfügbare online-Kurse (Kosmochemie kommt in den nächsten Tagen).')
