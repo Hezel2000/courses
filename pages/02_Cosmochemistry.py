6920cfc9106378c9216518f047d059d2e52e08cd
Dominik Hezel <dominik.hezel@em.uni-frankfurt.de>
2023-04-11 10:54:54 +0200

    x

--- pages/02_Cosmochemistry.py
+++ pages/02_Cosmochemistry.py
@@ -48,7 +48,10 @@ def useCourse(dfSearchAll):
 
         col1, col2 = st.columns([3, 1])
         with col1:
-            st.video(sel_row[0]['vimeo'])
+            if sel_row[0]['vimeo'] == 'vim':
+                st.video(sel_row[0]['youtube - deutsch'])
+            else:
+                st_player(sel_row[0]['vimeo'])
         with col2:
             st.write('Laufzeit: ' + sel_row[0]['Laufzeit'])
             with st.expander('Schlagworte', expanded=True):
