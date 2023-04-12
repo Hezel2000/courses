e832ce0e76c31f1fd7d3768754a3339359f1328e
Dominik Hezel <dominik.hezel@em.uni-frankfurt.de>
2023-04-12 21:12:45 +0200

    x

--- Welcome.py
+++ Welcome.py
@@ -5,7 +5,7 @@ st.subheader('Welcome')
 
 st.write('Über die Sidebar links erhälst Du Zugriff auf derzeit 3 verfügbare online-Kurse (Kosmochemie kommt in den nächsten Tagen).')
 
-st.write("Zurück zu [GeoPlatform](https://www.geoplatform.de)")
+st.write("Zurück zu [GeoPlatform](http://www.geoplatform.de)")
 
 st.sidebar.image('data_microanalysis/Goethe-Logo.jpg', width=150)
 st.sidebar.write("Viele Wege führen zum Erfolg.")
