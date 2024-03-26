d2dea51297446cf1b178439e5592f8513481d967
Dominik <dominik.hezel@em.uni-frankfurt.de>
2024-03-26 11:09:33 +0100

    update for Data Science 2024

--- pages/01_Data Science.py
+++ pages/01_Data Science.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import streamlit as st
-from st_aggrid import AgGrid, GridUpdateMode
-from st_aggrid.grid_options_builder import GridOptionsBuilder
+from datetime import datetime
 import pandas as pd
 
 # =============================================================================
@@ -24,71 +23,90 @@ st.write('*für Mineralogen, Kosmo-/Geochemiker, Petrologen & den ganzen Rest*')
 #---------------------------------#
 #------ Vorlesungen & Übungen ----#
 #---------------------------------#
-st.subheader('Wähle Deine Lerneinheit')
 
 @st.cache_data
 def importCourseDatasheet():
-    dfSearchAll= pd.read_csv('course_material_data_science.csv')
-    return dfSearchAll
+    dfSearchAll= pd.read_csv('data_science/course_material_data_science.csv')
+    df_weekly_chapters = pd.read_csv('data_science/weekly chapters.csv')
+    return dfSearchAll, df_weekly_chapters
 
 
 def useCourse(dfSearchAll):
-    dfSearchAll = dfSearchAll
-    gd = GridOptionsBuilder.from_dataframe(dfSearchAll)
-    gd.configure_pagination(enabled=True, paginationPageSize=5)
-    gd.configure_default_column(editable=True,groupable=True)
-    gd.configure_selection(selection_mode='single', use_checkbox=True)
-    gridoptions = gd.build()
-    grid_table = AgGrid(dfSearchAll, gridOptions=gridoptions, update_mode = GridUpdateMode.SELECTION_CHANGED, theme='material')
-    sel_row = grid_table['selected_rows']
+    col1, col2 = st.columns((20,80))
+    with col1:
+        chapter_sel = st.radio('Kapitelauswahl', range(1,10), horizontal = True)
+    with col2:
+        dfSearchAll_tmp = dfSearchAll[dfSearchAll['Kapitel'] == chapter_sel]
+        topic_sel = st.selectbox('Auswahl der Einheit', dfSearchAll_tmp['Titel'].tolist())
+        sel_row = dfSearchAll.loc[dfSearchAll[dfSearchAll['Titel'] == topic_sel].index[0]]
     
-    if len(sel_row) > 0:    
-        
+    if len(sel_row) > 0:
         col1, col2 = st.columns([3, 1])
         with col1:
-            st.video(sel_row[0]['Youtube'])
+            st.video(sel_row['Youtube'])
         with col2:
-            st.write('Laufzeit: ' + sel_row[0]['Laufzeit'])
+            st.write('Laufzeit: ' + sel_row['Laufzeit'])
             with st.expander('Jupyter Notebooks', expanded=True):
-                if sel_row[0]['Vorlesung ipynb'] != 'none':
-                    vorlesung = "[Vorlesung](https://raw.githubusercontent.com/Hezel2000/Data_Science/main/jupyter_nb/" + sel_row[0]['Vorlesung ipynb'] + ")"
+                if sel_row['Lecture ipynb'] != 'none':
+                    vorlesung = "[Vorlesung](jupyter_nb/" + sel_row['Lecture ipynb'] + ")"
                 else:
                     vorlesung=''
-                if sel_row[0]['Übungen ipynb'] != 'none':
-                    uebungen = "[Übungen](https://raw.githubusercontent.com/Hezel2000/Data_Science/main/jupyter_nb/" + sel_row[0]['Übungen ipynb'] + ")"
+                if sel_row['Exercise ipynb'] != 'none':
+                    uebungen = "[Übungen](jupyter_nb/" + sel_row['Exercise ipynb'] + ")"
                 else:
                     uebungen=''
-                if sel_row[0]['Lösungen ipynb'] != 'none':
-                    loesungen = "[Lösungen](https://raw.githubusercontent.com/Hezel2000/Data_Science/main/jupyter_nb/" + sel_row[0]['Lösungen ipynb'] + ")"
+                if sel_row['Solution ipynb'] != 'none':
+                    loesungen = "[Lösungen](jupyter_nb/" + sel_row['Solution ipynb'] + ")"
                 else:
                     loesungen=''
                 if vorlesung=='' and uebungen=='' and loesungen=='':
                     st.write('keine vorhanden')
                 else:
                     st.write(vorlesung,uebungen,loesungen)
+              
             with st.expander('Schlagworte', expanded=True):
-                if sel_row[0]['Schlagworte'] != 'none':
-                    st.write(sel_row[0]['Schlagworte'])
+                if sel_row['Schlagworte'] != 'none':
+                    st.write(sel_row['Schlagworte'])
                 else:
-                    st.write('keine vorhannden')
-        
-        #st.subheader('Beschreibung')
-        st.write(sel_row[0]['Beschreibung'])
+                    st.write('keine vorhanden')
 
-# =============================================================================
-#         else:
-#             st.subheader('Wähle eine Einheit aus obiger Liste')
-# =============================================================================
+        st.write(sel_row['Beschreibung'])
         
 
-dfSearchAll = importCourseDatasheet()
-useCourse(dfSearchAll)
+dfSearchAll, df_weekly_chapters = importCourseDatasheet()
+
+tab1, tab2, tab3 = st.tabs(['Lerneinheiten', 'Wochenübersicht', 'x'])
+
+with tab1:
+    df_weekly_dates = pd.DataFrame()
+    df_weekly_dates['pd Datum'] = pd.to_datetime(df_weekly_chapters['Datum'])
+    current_date = pd.to_datetime(datetime.now().date())
+    # -----------------
+    current_date = datetime(day=16, month=5, year=2024)
+    st.write(current_date)
+    # -----------------
+    current_week_index = df_weekly_dates.index[(df_weekly_dates['pd Datum'] >= current_date)]
+    current_week_info = df_weekly_chapters.iloc[current_week_index.tolist()[0]]
+    
+    with st.expander(f'Woche: {str(current_week_info["Woche"])} –   Thema: {str(current_week_info["Thema"])}'):
+        st.write('Erklärungen')
+
+    x=1
+    if x==1:
+        useCourse(dfSearchAll)
+
+with tab2:
+    st.dataframe(df_weekly_chapters, width=800, hide_index=True)
+
+with tab3:
+    st.write('x')
+
 
     
 #---------------------------------#
 #------ Main Page Sidebar --------#
 #---------------------------------#  
 
-st.sidebar.image('data/Goethe-Logo.jpg', width=150)
+st.sidebar.image('images/Goethe-Logo.jpg', width=150)
 st.sidebar.write("Viele Wege führen zum Erfolg.")
 
